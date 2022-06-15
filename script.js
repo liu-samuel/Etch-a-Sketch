560785dd22e7e80a41f212f132a8173d20b4c28e
liu-samuel <liusamuelt@gmail.com>
2022-06-15 17:09:28 -0400

    make restart and create change, make change work and it dynamically change

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
+let change = document.querySelector(".change");
 let container = document.getElementById("grid");
 let restart = document.querySelector('.restart');
 let cells = document.getElementsByClassName("cell");
 
-let rows = prompt("How many rows? (Up to 64)");
-let columns = prompt("How many columns? (Up to 64)");
-let numRows = parseInt(rows);
-let numColumns = parseInt(columns);
+let rows = 16;
+let columns = 16;
 let height = parseInt(container.style.height);
 let width = parseInt(container.style.width);
 
-let cellHeight = 640 / numRows;
-let cellWidth = 640 / numColumns;
+let cellHeight = 600 / rows;
+let cellWidth = 600 / columns;
 
 for (i = 0; i < rows * columns; i++) {
     let cell = document.createElement("div");
@@ -20,7 +19,7 @@ for (i = 0; i < rows * columns; i++) {
     cell.style.width = String(cellWidth) + 'px';
 
     cell.addEventListener("mouseenter", () => {
-        cell.style.backgroundColor = "red";
+        cell.style.backgroundColor = "grey";
         console.log("div mouse over");
     }); 
 
@@ -54,5 +53,41 @@ function randomColor() {
 restart.addEventListener("click", () => {
     for (let cellRestart of cells) {
         cellRestart.style.backgroundColor = "white";
+    }
+})
+
+change.addEventListener("click", () => {
+    rows = prompt("How many rows? (Up to 100)");
+    columns = prompt("How many columns? (Up to 100)");
+    numRows = parseInt(rows);
+    numColumns = parseInt(columns);
+
+    for (let cell of cells) {
+        container.removeChild(cell);
+    }
+
+    for (i = 0; i < rows * columns; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell);
+        cell.style.height = String(cellHeight) + 'px';
+        cell.style.width = String(cellWidth) + 'px';
+    
+        cell.addEventListener("mouseenter", () => {
+            cell.style.backgroundColor = "red";
+            console.log("div mouse over");
+        }); 
+    
+        /* cell.addEventListener("mouseout", () => {
+            cell.style.background = "white";
+            console.log("div mouse out")
+        })
+    
+        cell.addEventListener("click", () => {
+            color = randomColor();
+            cell.style.background = color;
+            console.log("div clicked");
+        });
+        */
     }
 })
