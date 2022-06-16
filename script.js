b121ab51f4a01dc5b00a5dac643a04871d220c5f
liu-samuel <liusamuelt@gmail.com>
2022-06-15 21:47:34 -0400

    layout working, add modes

--- script.js
+++ script.js
@@ -3,47 +3,25 @@ let container = document.getElementById("grid");
 let restart = document.querySelector('.restart');
 let cells = document.getElementsByClassName("cell");
 
-let rows = 16;
-let columns = 16;
+let numRows = 16;
+let numColumns = 16;
 let height = parseInt(container.style.height);
 let width = parseInt(container.style.width);
 
-let cellHeight = 600 / rows;
-let cellWidth = 600 / columns;
+let cellHeight = (640 - numColumns * 2) / numRows;
+let cellWidth = (640 - numRows * 2) / numColumns;
 
-for (i = 0; i < rows * columns; i++) {
+for (i = 0; i < numRows * numColumns; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
     container.appendChild(cell);
     cell.style.height = String(cellHeight) + 'px';
     cell.style.width = String(cellWidth) + 'px';
 
-    cell.addEventListener("mouseenter", () => {
-        cell.style.backgroundColor = "grey";
-        console.log("div mouse over");
-    }); 
-
-    /* cell.addEventListener("mouseout", () => {
-        cell.style.background = "white";
-        console.log("div mouse out")
-    })
-
-    cell.addEventListener("click", () => {
-        color = randomColor();
-        cell.style.background = color;
-        console.log("div clicked");
-    });
-    */
+    // addMouseOver(cell);
+    addMouseClick(cell);
+    // addMouseOut(cell);
 }
-
-/*
-function mouseOver(id) {
-    
-}
-
-function mouseClick(id) {
-    
-} */
 
 function randomColor() {
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -57,13 +35,16 @@ restart.addEventListener("click", () => {
 })
 
 change.addEventListener("click", () => {
-    rows = prompt("How many rows? (Up to 100)");
-    columns = prompt("How many columns? (Up to 100)");
+    let rows = prompt("How many rows? (Up to 100)");
+    let columns = prompt("How many columns? (Up to 100)");
     numRows = parseInt(rows);
     numColumns = parseInt(columns);
 
-    for (let cell of cells) {
-        container.removeChild(cell);
+    cellHeight = (640 - numColumns * 2) / rows;
+    cellWidth = (640 - numRows * 2) / columns;
+
+    while (cells[0]) {
+        container.removeChild(cells[0]);
     }
 
     for (i = 0; i < rows * columns; i++) {
@@ -72,22 +53,34 @@ change.addEventListener("click", () => {
         container.appendChild(cell);
         cell.style.height = String(cellHeight) + 'px';
         cell.style.width = String(cellWidth) + 'px';
-    
-        cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = "red";
-            console.log("div mouse over");
-        }); 
-    
-        /* cell.addEventListener("mouseout", () => {
-            cell.style.background = "white";
-            console.log("div mouse out")
-        })
-    
-        cell.addEventListener("click", () => {
-            color = randomColor();
-            cell.style.background = color;
-            console.log("div clicked");
-        });
-        */
+
+        // addMouseOver(cell);
+        addMouseClick(cell);
+        // addMouseOut(cell);
     }
 })
+
+function addMouseOver(cell) {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "red";
+        console.log("div mouse over"); 
+    });
+    return cell;
+}
+
+function addMouseOut(cell) {
+    cell.addEventListener("mouseout", () => {
+        cell.style.background = "white";
+        console.log("div mouse out")
+    });
+    return cell;
+}
+
+function addMouseClick(cell) {
+    cell.addEventListener("click", () => {
+        color = randomColor();
+        cell.style.background = color;
+        console.log("div clicked");
+    });
+    return cell;
+}
